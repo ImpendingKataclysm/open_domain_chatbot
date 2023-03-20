6938a2d3853b4340c39604a53f6ccb84c8878ecb
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-03-19 18:48:51 -0700

    Connected chatbot backend with chat window GUI

--- chatbot_window.py
+++ chatbot_window.py
@@ -1,4 +1,6 @@
 from PyQt6.QtWidgets import QMainWindow, QTextEdit, QLineEdit, QPushButton
+from chatbot import Chatbot
+import threading
 
 
 class ChatbotWindow(QMainWindow):
@@ -9,6 +11,8 @@ class ChatbotWindow(QMainWindow):
         super().__init__()
         self.setMinimumSize(700, 500)
 
+        self.chatbot = Chatbot()
+
         # Add chat area
         self.chat_area = QTextEdit(self)
         self.chat_area.setGeometry(10, 10, 480, 320)
@@ -17,9 +21,27 @@ class ChatbotWindow(QMainWindow):
         # Add input field
         self.input_field = QLineEdit(self)
         self.input_field.setGeometry(10, 340, 480, 40)
+        self.input_field.returnPressed.connect(self.send_message)
 
         # Add button
         self.button = QPushButton("Send", self)
         self.button.setGeometry(500, 340, 100, 40)
+        self.button.clicked.connect(self.send_message)
 
         self.show()
+
+    def send_message(self):
+        """
+        Send the user's input to the chatbot api
+        :return: None
+        """
+        user_input = self.input_field.text().strip()
+        self.chat_area.append(f"<p style='color:#333'>Me: {user_input}</p?")
+        self.input_field.clear()
+
+        thread = threading.Thread(target=self.get_bot_response, args=(user_input, ))
+        thread.start()
+
+    def get_bot_response(self, user_input):
+        response = self.chatbot.get_response(user_input)
+        self.chat_area.append(f"<p style='color:#333'>Chatbot: {response}</p>")
